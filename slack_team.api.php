40c011d59d023138596037daee3686ec412715ac
DarkPrinceWW <vk384d@gmail.com>
2016-04-10 14:57:40 +0300

    Init commit

---
+++ slack_team.api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Slack Team module.
+ */
+
+/**
+ * Alter messages before their display.
+ *
+ * @param array $messages
+ *   The messages of slack team.
+ */
+function hook_slack_team_view_history_alter(&$messages) {
+  // Code.
+}
